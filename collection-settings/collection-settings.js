ed8a099054560eec3d093d134ccf9b00437a407b
Yannic Brügger <yannic@bruegger.dev>
2022-08-09 12:30:42 +0200

    add collection settings logic

--- collection-settings/collection-settings.js
+++ collection-settings/collection-settings.js
@@ -1,4 +1,5 @@
-import { getCollection, getQuotesOfCollection } from '../lib/quotes-api.js';
+import { getCollection, getUsers, getCollectionModerators, getCollectionOwner } from '../lib/quotes-api.js';
+import { getToken } from '../lib/auth.js';
 
 const urlParameters = new URLSearchParams(window.location.search);
 
@@ -12,3 +13,25 @@ if (isNaN(collectionId)) throw new Error('Parameter \'id\' is not a number.');
 const collection = await getCollection(collectionId);
 
 document.getElementById('name').value = collection.attributes.name;
+
+const users = await getUsers(getToken());
+const moderators = await getCollectionModerators(collectionId, getToken());
+moderators.forEach((moderator) => {
+  const clone = document.querySelector('template#user').content.cloneNode(true)
+  clone.querySelector('.username').innerText = moderator.attributes.username;
+  clone.querySelector('.remove-moderator').addEventListener('click', (e) => {
+    // TODO: implement remove moderator functionality
+    console.log(moderator.attributes.username);
+    e.preventDefault();
+  });
+  document.querySelector('#moderators').appendChild(clone);
+});
+
+const owner = await getCollectionOwner(collectionId, getToken());
+document.getElementById('owner').value = owner.attributes.username;
+
+console.log({
+  users,
+  moderators,
+  owner,
+});
