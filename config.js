4ef3ca2b60d00821f815e813b4040091dfce6cdd
Yannic Brügger <yannic.bruegger@voss.net>
2022-09-15 18:37:14 +0200

    add proper url

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const ONE_DAY_IN_SECONDS = 60*60*24;
 
-export const API_URL = `${window.location.protocol}//h2964058.stratoserver.net:1337/api`;
+export const API_URL = `${window.location.protocol}//quotes-strapi.jankoll.online/api`;
 export const LOGIN_EXPIRATION_IN_SECONDS = ONE_DAY_IN_SECONDS;
