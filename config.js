72552c1368af3a56c444dfdc879f0ff6b05aa639
Yannic Brügger <yannic.bruegger@voss.net>
2022-09-15 18:27:17 +0200

    fix falty config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const ONE_DAY_IN_SECONDS = 606024;
+export const ONE_DAY_IN_SECONDS = 60*60*24;
 
-export const API_URL = 'http://h2964058.stratoserver.net:1337/api';
+export const API_URL = `${window.location.protocol}//h2964058.stratoserver.net:1337/api`;
 export const LOGIN_EXPIRATION_IN_SECONDS = ONE_DAY_IN_SECONDS;
