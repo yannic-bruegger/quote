3fe9ffbfc86bcda3bc773d67c9715f71601a80bf
Jan Koll <mail@jankoll.de>
2022-09-21 18:47:28 +0200

    Add quote deletion, Add deletion warning

--- edit-quote/edit-quote.js
+++ edit-quote/edit-quote.js
@@ -1,5 +1,5 @@
 import { getToken } from '../lib/auth.js';
-import { updateQuote, getSingleQuote } from '../lib/quotes-api.js';
+import { updateQuote, getSingleQuote, deleteQuote } from '../lib/quotes-api.js';
 
 const urlParameters = new URLSearchParams(window.location.search);
 
@@ -11,14 +11,12 @@ const quoteId = parseInt(quoteIdAsText, 10);
 if (isNaN(quoteId)) throw new Error('Parameter \'id\' is not a number.');
 
 const quote = await getSingleQuote(quoteId, getToken());
-
-console.log(quote.attributes.collection.data.id);
+const collectionId = quote.attributes.collection.data.id;
 
 document.getElementById('content').value = quote.attributes.content;
 document.getElementById('quoted').value = quote.attributes.quoted;
 
 async function tryUpdateQuote() {
-  const collectionId = quote.attributes.collection.data.id;
   const content = document.querySelector('#content').value;
   const quoted = document.querySelector('#quoted').value;
   console.log(await updateQuote(getToken(), {quoteId, content, quoted, collectionId}));
@@ -30,11 +28,15 @@ document.querySelector('#edit-quote').addEventListener('submit', (event) => {
   tryUpdateQuote()
 });
 
-// const reply = await getMyCollections(getToken());
+document.querySelector('#delete').addEventListener('click', async (e) => {
+    e.preventDefault();
+    if (confirm('Are you sure you want to delete this quote?')) {
+        await deleteQuote(quoteId, getToken());
+        window.location.href = `/collection/?id=${collectionId}`;
+    }
+});
 
-// for (const collection of (reply.owns.concat(reply.moderates))) {
-//   const option = document.createElement('option');
-//   option.value = collection.id;
-//   option.innerText = collection.name;
-//   document.querySelector('#collection').appendChild(option);
-// }
+document.querySelector('#cancel').addEventListener('click', async (e) => {
+    e.preventDefault();
+    window.location.href = `/collection/?id=${collectionId}&quoteId=${quoteId}`;
+});
