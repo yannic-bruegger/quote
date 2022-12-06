cf3684557e18544629616d38037d936ea47c982d
Jan Koll <mail@jankoll.de>
2022-12-06 12:00:00 +0100

    Update Config

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export const ONE_DAY_IN_SECONDS = 60*60*24;
-export const API_URL = 'https://quotes-strapi.jankoll.online/api';
+export const API_URL = 'http://strapi.quoteapp.de/api';
 export const LOGIN_EXPIRATION_IN_SECONDS = ONE_DAY_IN_SECONDS * 365;
