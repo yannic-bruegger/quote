e05ce7a128bba4d3082ec17865f12dcbaa5f0d22
Yannic Brügger <yannic@bruegger.dev>
2023-09-02 16:53:22 +0200

    feat: switch from github pages to real dev server

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,17 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
 
-	kit: {
-      adapter: adapter(),
-      paths: {
-        base: dev ? '' : process.env.BASE_PATH,
-      }
-    }
+  kit: {
+    adapter: adapter(),
+  }
 };
 
 export default config;
