4e119fcff7bb1bb5df012ac9060983ab4a03c80d
jan.koll <mail@jankoll.de>
2024-04-20 16:26:59 +0200

    Add error pages

---
+++ src/components/error-page.svelte
@@ -0,0 +1,37 @@
+<div class="error-container">
+	<div class="contents">
+	  <h1 class="colored-text pink-gradient">404</h1>
+	  <h2 class="">Page Not Found</h2>
+	  <p><a href="/" class="colored-text blue-gradient">Klick here to go home</a></p>
+	</div>
+  </div>
+  
+  <style scoped>
+	.error-container {
+	  display: flex;
+	  flex-direction: column;
+	  align-items: center;
+	  justify-content: center;
+	  height: 100%;
+	  background-image: url('background.svg');
+	  background-size: cover;
+	  background-position: center center;
+	}
+  
+	.contents {
+	  display: flex;
+	  flex-direction: column;
+	  gap: 1rem;
+	  text-align: center;
+	  min-width: 20rem;
+	}
+  
+	h1 {
+	  font-weight: 700;
+	  font-size: 4rem;
+	}
+
+	h2 {
+		font-weight: 100;
+	}
+  </style>
