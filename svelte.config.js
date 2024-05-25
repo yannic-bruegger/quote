81765d44b569fabc087df57d3d7c9d7fa4aeacce
Yannic Brügger <yannic.bruegger@gmail.com>
2024-05-25 17:28:06 +0200

    make app installable

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-
+  
   kit: {
     adapter: adapter(),
   }
